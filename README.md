f2bc104bd87a68c4126dfa7e650e4081cd755d80
Kirwan Elmsly <kirwan.elmsly@gmail.com>
2018-04-20 22:31:01 +1000

    Added form input for language rather than being hardcoded as Python.

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # beginner-issues
+Under Development & Incomplete.
+
 Simple Python script to aggregate GitHub issue search results for multiple labels.
 
-So I couldn't figure out how to include multiple labels while searching GitHub issues so I quickly wrote up a Python script to aggregate results for multiple labels.
+So I couldn't figure out how to include multiple labels while searching GitHub issues so I quickly wrote up a Python script to aggregate results for multiple labels. Currently making it work as a simple web app with a Flask back end. I haven't really done much web development in the past so this is a learning process for me. This might take a while too as I don't have much time to work on it.
 
-Feel free to message me with feedback or if you think this is stupid.
+Feedback is appreciated! Feel free to message me with feedback or if you think this is stupid.
 
 
 # Requirements
 Developed using Python 3.6 but it should work on Python 3.x
 
-Also depends on Requests: http://docs.python-requests.org/en/master/
+Additional requirements can be found in requirements.txt
--- beginner_issues.py
+++ beginner_issues.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from requests import get
 from time import sleep
 
@@ -14,32 +14,34 @@ def get_items_from_txt(filepath):
     return items
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    NUM_RESULTS = 100
-    SEARCHES_PER_MINUTE = 10
-    language = "python"
+    if request.method == 'POST':
+        NUM_RESULTS = 100
+        SEARCHES_PER_MINUTE = 10
+        language = request.form.get("language")
 
-    dirname = os.path.dirname(__file__)
-    labels_path = os.path.join(dirname, 'labels.txt')
-    labels = get_items_from_txt(labels_path)
+        dirname = os.path.dirname(__file__)
+        labels_path = os.path.join(dirname, 'labels.txt')
+        labels = get_items_from_txt(labels_path)
 
-    issues = []
+        issues = []
+        for label in labels:
+            url = "https://api.github.com/search/issues?q=label:{}+language:{}+state:open&sort=created".format(label, language)
+            response = get(url)
+            data = response.json()
+            for item in data['items']:
+                if item not in issues:
+                    issues.append(item)
+            #GitHub search API has limit of 10 searches per minute (Unauthenticated)
+            #Sleep to avoid being rejected.
+            sleep(60/SEARCHES_PER_MINUTE)
 
-    for label in labels:
-        response = get("https://api.github.com/search/issues?q=label:" +
-                                label + "+language:" + language + "+state:open&sort=created")
-        data = response.json()
-        for item in data['items']:
-            if item not in issues:
-                issues.append(item)
-        #GitHub search API has limit of 10 searches per minute (Unauthenticated)
-        #Sleep to avoid being rejected.
-        sleep(60/SEARCHES_PER_MINUTE)
+        issues_sorted = sorted(issues, key=lambda issue: issue['created_at'], reverse=True)
 
-    issues_sorted = sorted(issues, key=lambda issue: issue['created_at'], reverse=True)
-
-    return render_template('main.html', issues_list=issues_sorted)
+        return render_template('main.html', issues_list=issues_sorted)
+    elif request.method == 'GET':
+        return render_template('form.html')
 
 
 if __name__ == "__main__":
