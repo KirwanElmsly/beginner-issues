b06eadf03812dfe99db27d64898cea35094bd69e
Kirwan Elmsly <kirwan.elmsly@gmail.com>
2018-04-22 14:37:29 +1000

    Minor updates and refactoring

--- beginner_issues.py
+++ beginner_issues.py
@@ -1,40 +1,51 @@
 from flask import Flask, render_template, request
-from requests import get
 from time import sleep
 
+import requests
 import json
 import os
 
 app = Flask(__name__)
 
-def get_items_from_txt(filepath):
-    reader = open(filepath, 'r')
+def get_items_from_txt(dir, file):
+    path = os.path.join(dir, file)
+    reader = open(file, 'r')
     items = [s.rstrip() for s in reader.readlines()]
     reader.close()
     return items
 
+def searches_per_minute(auth_string):
+    return requests.get("https://api.github.com/rate_limit" + auth_string).json()['resources']['search']['limit']
+
+def searches_remaining(auth_string):
+    return requests.get("https://api.github.com/rate_limit" + auth_string).json()['resources']['search']['remaining']
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'POST':
-        NUM_RESULTS = 100
-        SEARCHES_PER_MINUTE = 10
-        language = request.form.get("language")
+    dirname = os.path.dirname(__file__)
 
-        dirname = os.path.dirname(__file__)
-        labels_path = os.path.join(dirname, 'labels.txt')
-        labels = get_items_from_txt(labels_path)
+    labels = get_items_from_txt(dirname, 'labels.txt')
+    credentials = get_items_from_txt(dirname, 'credentials.txt') #Client ID & Client Secret are stored here
+    CLIENT_ID = credentials[0]
+    CLIENT_SECRET = credentials[1]
+    auth_string = "?client_id={}&client_secret={}".format(CLIENT_ID, CLIENT_SECRET)
+
+    #SEARCHES_PER_MINUTE = searches_per_minute(auth_string)
+    SEARCHES_PER_MINUTE = 10
+
+    if request.method == 'POST':
+        language = request.form.get("language")
 
         issues = []
         for label in labels:
-            url = "https://api.github.com/search/issues?q=label:{}+language:{}+state:open&sort=created".format(label, language)
-            response = get(url)
+
+            url = "https://api.github.com/search/issues?q=label:{}+language:{}+state:open&sort=created{}".format(label, language, auth_string)
+            response = requests.get(url)
             data = response.json()
             for item in data['items']:
                 if item not in issues:
                     issues.append(item)
-            #GitHub search API has limit of 10 searches per minute (Unauthenticated)
-            #Sleep to avoid being rejected.
             sleep(60/SEARCHES_PER_MINUTE)
 
         issues_sorted = sorted(issues, key=lambda issue: issue['created_at'], reverse=True)
