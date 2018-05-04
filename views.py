cde7e37e73e11e42fea159374c205695ea9ad7bb
Kirwan Elmsly <kirwan.elmsly@gmail.com>
2018-05-04 21:41:11 +1000

    Got Bootstrappy. Results are now responsive. Also changed one line in views.py tinkering with github login function.

--- views.py
+++ views.py
@@ -35,4 +35,4 @@ def login():
         return redirect(url_for("github.login"))
     resp = github.get("/user")
     assert resp.ok
-    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return redirect(url_for('index'))
