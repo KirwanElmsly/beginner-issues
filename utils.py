6313abbcc0440f818d55cd841a9a681b58a56811
Kirwan Elmsly <kirwan.elmsly@gmail.com>
2018-05-09 14:02:56 +1000

    Added DocStrings to utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import requests
 from flask_dance.contrib.github import github
 
 def get_items_from_txt(dir, file):
+    """Gets list of labels from a text file."""
     path = os.path.join(dir, file)
     reader = open(path, 'r')
     items = [s.rstrip() for s in reader.readlines()]
@@ -10,12 +11,20 @@ def get_items_from_txt(dir, file):
     return items
 
 def searches_per_minute():
+    """Returns number of searches available per minute"""
     return github.get("https://api.github.com/rate_limit").json()['resources']['search']['limit']
 
 def searches_remaining():
+    """Returns number of searches remaining"""
     return github.get("https://api.github.com/rate_limit").json()['resources']['search']['remaining']
 
 def github_search(labels, language):
+    """Searches GitHub Issues for given label and language.
+
+    Keyword arguments:
+    labels -- list of labels
+    language -- string containing language to search
+    """
     issues = []
     for label in labels:
         url = ("/search/issues?q=label:{}+language:{}+state:open&sort=created"
