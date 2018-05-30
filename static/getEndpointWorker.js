b653cc8d3b01baa60b76897c87ac2d72085c2147
Kirwan Elmsly <kirwan.elmsly@gmail.com>
2018-05-30 21:38:15 +1000

    Removed unneeded console.log's

--- static/getEndpointWorker.js
+++ static/getEndpointWorker.js
@@ -1,7 +1,6 @@
 
 onmessage = function(e) {
     url = e.data;
-    console.log("Received " + url);
     var request = new XMLHttpRequest();
     request.open('GET', url, false);
 
@@ -9,7 +8,6 @@ onmessage = function(e) {
     if (request.status >= 200 && request.status < 400) {
       // Success!
       var data = request.responseText
-      console.log(request);
       postMessage(data);
     } else {
       // We reached our target server, but it returned an error - deal with it?
